9c97c52652c076af49f300b9567a50dc0fa604e4
Gustavo <gustavorodrigues.grm@gmail.com>
2022-10-24 15:37:57 -0300

    exclusão de arquivos desnecessários

--- src/controllers/temp.js
+++
@@ -1,8 +0,0 @@
-const { batalhar } = require("../service/batalhaService");
-
-const batalha = async (req, res) => {
-  const { pokemonAId } = req.params;
-  const { pokemonBId } = req.params;
-};
-
-module.exports = { batalha };
